d9d73dea7a9953ea5d8a2f34b97537d4b99a494d
Gabriela <gabriela.araujo@fcamara.com.br>
2020-06-04 07:27:29 -0300

    first commit with project files

---
+++ src/test/java/com/example/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
